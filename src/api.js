b9371687c35579e518f19b12e300ebdf19c215ad
dhivya-7 <dhivyadhivakar1996@gmail.com>
2025-09-13 17:15:18 +0530

    Update api.js to connect to live backend

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
+//const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
 const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
-
 // Login
 export async function login(payload) {
   try {
